c2edcee2e5525c79643178fc72b3b0c08dc2936e
ali-85 <alidavit85@gmail.com>
2023-04-18 10:40:13 +0700

    Sidebar akses

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -9,6 +9,7 @@
                     <i class="icon-home menu-icon"></i><span class="nav-text">Dashboard</span>
                 </a>
             </li>
+            @if (in_array(Auth::user()->role_id, [1, 3]))            
             <li>
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                     <i class="fa fa-database"></i> <span class="nav-text">Master Data</span>
@@ -22,6 +23,8 @@
                     <li><a href="{{ route('holiday.index') }}"><i class="fa-solid fa-calendar-day"></i> Holidays Date</a></li>
                 </ul>
             </li>
+            @endif
+            @if (in_array(Auth::user()->role_id, [1, 2]))            
             <li>
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                     <i class="fa fa-microchip"></i> <span class="nav-text">Master Devices</span>
@@ -34,6 +37,8 @@
                     <li><a href="{{ route('schedule.index') }}"><i class="icon-notebook"></i> Schedule Management</a></li>
                 </ul>
             </li>
+            @endif
+            @if (in_array(Auth::user()->role_id, [1, 4]))
             <li>
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                     <i class="icon-user-following"></i> <span class="nav-text">Management Attendances</span>
@@ -49,7 +54,9 @@
                 <ul aria-expanded="false">
                     <li><a href="{{ route('payroll.employee.list') }}"><i class="icon-note"></i> Payroll</a></li>
                 </ul>
-            </li>
+            </li>                
+            @endif
+            @if (in_array(Auth::user()->role_id, [1, 2]))            
             <li>
                 <a class="has-arrow" href="javascript:void()" aria-expanded="false">
                     <i class="icon-printer"></i> <span class="nav-text">Reports</span>
@@ -60,6 +67,7 @@
                     <li><a href="{{ route('outmonitoring.index') }}"><i class="icon-list"></i> Out Room Report</a></li>
                 </ul>
             </li>
+            @endif
             <li>
                 <a href="{{ route('auth.logout') }}" aria-expanded="false">
                     <i class="icon-logout menu-icon"></i><span class="nav-text">Logout</span>
